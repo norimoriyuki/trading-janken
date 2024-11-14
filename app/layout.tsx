b07ffab0a4ef36e6c8d79ad8829d307fcfdd46e8
unknown <msuryrsa@gmail.com>
2024-11-14 13:09:27 +0900

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: 'Trading Janken',
@@ -13,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <Head>
+      <Analytics/>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-P01LS0XE5T"></script>
         <script
           dangerouslySetInnerHTML={{
